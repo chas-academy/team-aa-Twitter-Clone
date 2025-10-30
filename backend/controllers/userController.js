ca28f5858cf2e64a79e254e602771eeee5bd7c39
Atilla1907 <atilla.gorgulu@chasacademy.se>
2025-10-30 12:56:11 +0100

    usercontrollers

---
+++ backend/controllers/userController.js
@@ -0,0 +1,45 @@
+const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
+const generateToken = (id, role) => {
+    return jwt.sign({ id, role }, process.env.JWT_SECRET, { expiresIn: '3d' });
+};
+
+exports.registerUser = async (req, res) => {
+    try {
+        const { username, email, password } = req.body;
+
+        const userExists = await User.findOne({ email });
+        if (userExists) return res.status(400).json({ message: 'Email is already in use' });
+
+        const user = await User.create({ username, email, password });
+        res.status(201).json({
+            _id: user._id,
+            username: user.username,
+            email: user.email,
+            token : generateToken(user._id, user.role)
+        });
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+};
+
+exports.loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+
+        if (suer && (await user.matchPassword(password))) {
+            res.json({
+                _id: user._id,
+                username: user.username,
+                email: user.email,
+                token: generateToken(user._id, user.role)
+            });
+        } else {
+            res.status(401).json({ message: 'Invalid email or password' });
+        }
+    } catch (error) {
+        res.status(500).json({ message: 'Server error' });
+    }
+};
