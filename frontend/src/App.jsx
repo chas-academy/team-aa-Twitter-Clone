a86e93d0eb0b1177be7a6f05c2243e366bfdb9e4
Atilla1907 <atilla.gorgulu@chasacademy.se>
2025-11-05 09:16:56 +0100

    uppdated routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,39 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar';
 import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
+import AdminDashboard from './pages/AdminDashboard';
 
-function App() {
+   function App() {
   return (
     <AuthProvider>
       <Router>
         <Navbar />
         <Routes>
+          
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/admin"
+            element={
+              <ProtectedRoute adminOnly={true}>
+                <AdminDashboard />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </Router>
     </AuthProvider>
